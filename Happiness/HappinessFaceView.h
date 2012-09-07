1954874acec8aa7885bdcd730a074e3579930ea5
Brian Jenkins <bj@jenkinz.com>
2012-09-07 12:32:48 -0700

    Add HappinessFaceView view

---
+++ Happiness/HappinessFaceView.h
@@ -0,0 +1,13 @@
+//
+//  HappinessFaceView.h
+//  Happiness
+//
+//  Created by Brian Jenkins on 9/7/12.
+//  Copyright (c) 2012 Brian Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HappinessFaceView : UIView
+
+@end
---
+++ Happiness/HappinessFaceView.m
@@ -0,0 +1,31 @@
+//
+//  HappinessFaceView.m
+//  Happiness
+//
+//  Created by Brian Jenkins on 9/7/12.
+//  Copyright (c) 2012 Brian Jenkins. All rights reserved.
+//
+
+#import "HappinessFaceView.h"
+
+@implementation HappinessFaceView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
