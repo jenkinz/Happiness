1f47bd232a725e8148229bf25c8871e499bc8daf
Brian Jenkins <bj@jenkinz.com>
2012-09-07 12:38:23 -0700

    Init storyboard and hookup to HappinessViewController

--- Happiness/HappinessViewController.m
+++ Happiness/HappinessViewController.m
@@ -7,13 +7,23 @@
 //
 
 #import "HappinessViewController.h"
+#import "HappinessFaceView.h"
 
 @interface HappinessViewController ()
+
+@property (nonatomic, weak) IBOutlet HappinessFaceView *faceView;
 
 @end
 
 @implementation HappinessViewController
 
 @synthesize happiness = _happiness;
+@synthesize faceView = _faceView;
+
+- (void)setHappiness:(int)happiness
+{
+    _happiness = happiness;
+    [self.faceView setNeedsDisplay]; // anytime `happiness` gets set, redraw the view
+}
 
 @end
